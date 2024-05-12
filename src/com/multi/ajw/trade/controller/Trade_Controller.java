5df2b2e22d439a06be863097b770729ea3aa7613
ajw <anjy0821@naver.com>
2024-05-12 17:54:20 +0900

    대략적인 기능 구현 완료 더 해야할 일 가격에 따른 point 지급, 데이터베이스 정리(DB수정 및 JOIN, 그에 맞춰 쿼리문 수정)

---
+++ src/com/multi/ajw/trade/controller/Trade_Controller.java
@@ -0,0 +1,55 @@
+package com.multi.ajw.trade.controller;
+
+import com.multi.ajw.trade.model.dao.Trade_Dao;
+import com.multi.ajw.trade.model.dto.OrderList;
+import com.multi.ajw.trade.model.dto.UserList;
+import com.multi.ajw.trade.service.TradeService;
+import com.multi.ajw.trade.view.Trade_add;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Trade_Controller {
+    private static TradeService tradeService = new TradeService();
+    public static void trade_regist() {//구매내역 조회 후 보상판매 신청
+        Trade_add add = new Trade_add();
+        //구매 목록 리스트 후 선택, 선택한 번호 등록
+        ArrayList<OrderList> list;
+        list = tradeService.order_selectAll();
+        if(!list.isEmpty()){
+            add.displayOrderList(list);
+            tradeService.add();
+        }else{
+            System.out.println("신청 목록이 없습니다.");
+        }
+    }
+
+    public static void show_user_list() {//보상판매 신청 유저 조회
+        Trade_add add = new Trade_add();
+        ArrayList<UserList> list;
+        ArrayList<UserList> app_list;
+        list= tradeService.application_selectAll();
+        if(!list.isEmpty()){
+            add.displayUSERList(list);
+            app_list=tradeService.app_list();
+            if(!app_list.isEmpty()){
+                add.displayAPPList(app_list);
+                tradeService.judge();
+            }
+        }else{
+            System.out.println("신청 목록이 없습니다.");
+        }
+    }
+
+    public static void check_trade() {//유저가 본인이 신청한 결과 확인
+        Trade_add add = new Trade_add();
+        //구매 목록 리스트 후 선택, 선택한 번호 등록
+        ArrayList<UserList> list;
+        list = tradeService.is_approved();
+        if(!list.isEmpty()){
+            add.displayAPPList(list);
+        }else{
+            System.out.println("신청 목록이 없습니다.");
+        }
+    }
+}
